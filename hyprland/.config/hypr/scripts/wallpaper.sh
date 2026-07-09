7d319e4a1d60bce7ddcc45e22797f37f5737b61e
knownblackhat <knownblackhat@gmail.com>
2026-07-10 01:13:33 +0530

    fix: input video namespace issue

--- hyprland/.config/hypr/scripts/wallpaper.sh
+++ hyprland/.config/hypr/scripts/wallpaper.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/bash
 
-wallpapers=$(ls ~/.live_wallpaper/ | shuf| shuf|tr "\n" " ")
+wallpapers=$(ls ~/.live_wallpaper/ | shuf| shuf | tr "\n" "^")
 
-wp1=$(echo $wallpapers | awk '{print $1}')
-wp2=$(echo $wallpapers | awk '{print $2}')
-wp3=$(echo $wallpapers | awk '{print $3}')
+wp1=$(echo $wallpapers | awk -F^ '{print $1}')
+wp2=$(echo $wallpapers | awk -F^ '{print $2}')
+wp3=$(echo $wallpapers | awk -F^ '{print $3}')
+
+echo $wp1
+echo $wp2
+echo $wp3
 
 mpvpaper --fork -o "no-audio --cache=yes --demuxer-max-bytes=10MiB --hwdec=auto loop panscan=1.0" 'Chimei Innolux Corporation 0x1621 ' "~/.live_wallpaper/$wp1"
 mpvpaper --fork DP-1 -o "no-audio --cache=yes --demuxer-max-bytes=10MiB --hwdec=auto loop" "~/.live_wallpaper/$wp2"
